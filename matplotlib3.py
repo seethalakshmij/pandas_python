0e09d5e044e4183a034ae9bbc87cf52fc60c20c8
seethalakshmij <seethajaisi2828@gmail.com>
2026-09-15 21:24:26 +0530

    modified the file

--- matplotlib3.py
+++ matplotlib3.py
@@ -1,3 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+
+
+#A simple graph markdown
+
+x = [1,2,3,4,5]
+y = [1,2,3,4,5]
+
+plt.plot(x,y)
+
+plt.xlabel('X-axix')
+plt.ylabel("Y-axis")
+plt.title("Sample seetha graph")
+
+plt.show()

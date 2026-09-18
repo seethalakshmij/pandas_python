910484d39f73fbc88acad6270e58b967efd12596
seethalakshmij <seethajaisi2828@gmail.com>
2026-09-18 19:45:06 +0530

    added

--- matplotlib3.py
+++ matplotlib3.py
@@ -99,3 +99,15 @@ plt.ylabel("Y-axis data")
 plt.title("Filled Area Between Two Lines")
 plt.legend()
 plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fruits = ['Apples', 'Bananas', 'Cherries', 'Dates']
+sales = [400, 350, 300, 450]
+
+plt.bar(fruits, sales, color='violet')
+plt.title('Fruit Sales')
+plt.xlabel('Fruits')
+plt.ylabel('Sales')
+plt.show()
